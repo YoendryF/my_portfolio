b9ef0a8b41b0eb0c4529fcff5b478a7428378fac
Yoendry <yoendryferro2003@gmail.com>
2025-07-05 18:54:09 -0400

    adding files

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
-
-const isGithubPages = process.env.NODE_ENV === 'production';
-const repo = 'my-portfolio'; // your GitHub repo name
-
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  ...(isGithubPages && {
-    basePath: `/${repo}`,
-    assetPrefix: `/${repo}/`,
-  }),
 };
 
 module.exports = nextConfig;
