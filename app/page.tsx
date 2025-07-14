6da945f65587697d7ab9a1a3b1d8c0c5c26f3a48
Yoendry <yoendryferro2003@gmail.com>
2025-07-14 18:13:30 -0400

    Fixing double sidebar issue

--- app/page.tsx
+++ app/page.tsx
@@ -7,37 +7,6 @@ import "../styles/globals.css";
 export default function Home() {
   return (
     <div id="top" className="portfolio-layout">
-      {/* Sidebar */}
-      <aside className="sidebar">
-        <div className="sidebar-top">
-          <h2>Yoendry Ferro Santizo</h2>
-          <div className="profile-img-wrapper">
-            <Image
-              src="/photo1.jpg"
-              alt="Yoendry's Photo"
-              width={100}
-              height={100}
-              className="profile-img"
-            />
-          </div>
-          <p>Hi, I’m Yoendry, an aspiring software engineer. Welcome to my website</p>
-        </div>
-        <nav className="nav-links">
-          <a href="#top">Intro</a>
-          <a href="#projects">Projects</a>
-          <a href="#resume">Resume</a>
-          <a href="#contact">Contact</a>
-          <a href="#Education">Education</a>
-          <a href="#Skills">Skills</a>
-          <a href="#Experiences/Extracurriculars">Experiences</a>
-          <a href="#bio">Bio</a>
-        </nav>
-        <div className="social-links">
-          <a href="https://Linkedin.com/in/Yoendryferro">LinkedIn</a>
-          <a href="https://github.com/yoendryf">GitHub</a>
-        </div>
-      </aside>
-
       {/* Main content wrapper */}
       <main className="main-content">
         <section className="intro-card">
