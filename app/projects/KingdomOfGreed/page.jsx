36384c0ed8c7a2cb4579e121ee1652b599ec3bbb
Yoendry <yoendryferro2003@gmail.com>
2025-07-17 13:07:20 -0400

    Resizing KOG

--- app/projects/KingdomOfGreed/page.jsx
+++ app/projects/KingdomOfGreed/page.jsx
@@ -2,9 +2,9 @@ import styles from './KingdomOfGreedPage.module.css';
 
 export default function KingdomOfGreedPage() {
     return (
+      <div className={styles.pageWrapper}>
       <div className ={styles.fullBackground}>
       <main className={styles.kingdomMain}>
-        <div className={styles.pageWrapper}>
         <h1 className={styles.Title}>Kingdom Of Greed: Unity Dungeon Crawler</h1>
         {/*Title Screen, to the left side of the screen*/} {/*Intro Paragraph, to the right side of the screen*/}
         {/*Presentation youtube video, to the left side of the screen*/}{/*Animations, to the right side of the screen*/}
@@ -18,8 +18,8 @@ export default function KingdomOfGreedPage() {
         {/*Concept Drawings, Aseprite sheets*/}
         
         {/*Game download*/}
-        </div>
       </main>
+      </div>
       </div>
     );
   }
