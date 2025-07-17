d62402c7c39fd27dbf19d670f1af0ed2a4dc7c32
Yoendry <yoendryferro2003@gmail.com>
2025-07-17 18:16:08 -0400

    Adding KOG title screen

--- app/projects/KingdomOfGreed/page.jsx
+++ app/projects/KingdomOfGreed/page.jsx
@@ -13,6 +13,10 @@ export default function KingdomOfGreedPage() {
     <main className="kingdomMain">
       <h1 className="KOGTitle">Kingdom Of Greed: Unity Dungeon Crawler</h1>
     {/*Title Screen, to the left side of the screen*/} {/*Intro Paragraph, to the right side of the screen*/}
+      <div className = "KOGTitleScreen">
+        <img src="/TitleScreen.png" alt="Title Screen" />
+
+      </div>
         {/*Presentation youtube video, to the left side of the screen*/}{/*Animations, to the right side of the screen*/}
 
         {/*Tech Stack, in the middle with list items*/}
