98e3326af8b39f6249141a03266582f68f97831d
Yoendry <yoendryferro2003@gmail.com>
2025-07-20 17:08:02 -0400

    Editing favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
