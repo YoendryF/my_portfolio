a68f25055691a1f295ac2c6dda8b99d85dfc7dfc
Yoendry <yoendryferro2003@gmail.com>
2025-07-23 14:41:56 -0400

    Adding KOG youtube video

--- app/projects/KingdomOfGreed/page.jsx
+++ app/projects/KingdomOfGreed/page.jsx
@@ -19,6 +19,16 @@ export default function KingdomOfGreedPage() {
         </div>
         {/*Title Screen, to the left side of the screen*/} {/*Intro Paragraph, to the right side of the screen*/}
         <div className="KOGIntroSection">
+          <div className={styles.videoContainer}>
+            <iframe
+              width="560"
+              height="315"
+              src="https://youtu.be/omh0o22cYEQ"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
           <img src="/TitleScreen.png" alt="Title Screen" className = "KOGTitleScreen"/>
           <p className="KOGDescription">
             <strong>Kingdom of Greed</strong> begins with a goblin returning from an adventure, only to discover that his homeland has been overtaken by a tyrannical king.<br />
